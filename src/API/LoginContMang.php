cf15d2820bbfece232c89931f608dffd7e962edc
calebmack <156357219+calebmack@users.noreply.github.com>
2024-09-06 15:42:12 -0400

    Update LoginContMang.php

--- src/API/LoginContMang.php
+++ src/API/LoginContMang.php
@@ -7,7 +7,7 @@
 	$firstName = "";
 	$lastName = "";
 
-    $conn = new mysqli("localhost???", "ContactMangerUser", "ContactManagerPassword", "ContactManager");
+    $conn = new mysqli("localhost", "ContactMangerUser", "ContactManagerPassword", "contact_manager");
     if( $conn->connect_error )
 	{
 		returnWithError( $conn->connect_error );
