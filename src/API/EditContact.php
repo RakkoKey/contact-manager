8470b5496538e315d343ca8f40ca073864ab57dc
Lucas-DeReamer <167575863+Lucas-DeReamer@users.noreply.github.com>
2024-09-10 10:10:59 -0400

    Create EditContact.php

---
+++ src/API/EditContact.php
@@ -0,0 +1,23 @@
+<?php
+    $inData = getRequestInfo();
+
+    
+
+
+  function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+?>
