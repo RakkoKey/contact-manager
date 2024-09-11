522f9a1366d2928cae27b55dc8b4040ccd2b6c76
Lucas-DeReamer <167575863+Lucas-DeReamer@users.noreply.github.com>
2024-09-11 11:14:34 -0400

    Update GetContact.php
    Changed functionality to get ALL contacts, used when displaying contacts once logged in.

--- src/API/GetContact.php
+++ src/API/GetContact.php
@@ -1,10 +1,12 @@
 <?php
+	//used to get all contacts from a particular user
+
     $inData = getRequestInfo();
 
     $userID =  $inData["userID"];
-    $firstName = $inData["firstName"];
-    $lastName = $inData["lastName"];
-    $phoneNumber = $inData["phoneNumber"];
+
+    $resCount = 0;
+    $searchResults = "";
 
     $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "contact_manager");
     if( $conn->connect_error )
@@ -13,21 +15,40 @@
 	}
     else
     {
-        $stmt = $conn->prepare("SELECT FROM Contacts WHERE userID=? AND firstName=? AND lastName=? AND phoneNumber=?");
-        $stmt->bind_param("isss", $userID, $firstName, $lastName, $phoneNumber);
+        $stmt = $conn->prepare("SELECT FROM Contacts WHERE userID=?");
+        $stmt->bind_param("i", $userID);
         $stmt->execute();
 
-        if ($stmt->affected_rows > 0) {
-            returnWithInfo( $phoneNumber, $firstName, $lastName );
-        }
-        else {
-            returnWithError("Contact not found");
-        }
+	$result = $stmt->get_result();
+
+	while($row = $result->fetch_assoc())
+	{
+		if( $resCount > 0 )
+		{
+			$searchResults .= ",";
+		}
+		$resCount++;
+		$searchResults .= '{"firstName":"' . $row["firstName"] . '",';
+		$searchResults .= '"lastName":"' . $row["lastName"] . '",';
+		$searchResults .= '"phoneNumber":"' . $row["phoneNumber"] . '",';
+		$searchResults .= '"email":"' . $row["email"] . '",';
+		$searchResults .= '"address":"' . $row["address"] . '"}';
+	}
+
+	if( $resCount == 0 )
+	{
+		returnWithError( "No Contacts Found" );
+	}
+	else
+	{
+		returnWithInfo( $searchResults, $resCount );
+	}
 
 
 		$stmt->close();
 		$conn->close();
     }
+
 
   function getRequestInfo()
 	{
@@ -45,4 +66,11 @@
 		$retValue = '{"error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
+
+	function returnWithInfo( $searchResults, $num )
+	{
+		$retValue = '{"results":[' . $searchResults . '],"num":' . $num . '}';
+		sendResultInfoAsJson( $retValue );
+	}
+
 ?>
