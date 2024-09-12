48a1dfd78e7a8e70c06e96e04fc0aa77af625b50
EitanMir <eitanmiron234@gmail.com>
2024-09-12 17:24:13 -0400

    Added prototype for editContact() and removeContact()

--- index.js
+++ index.js
@@ -154,8 +154,17 @@ function addContact() {
 
 /* Function to remove a contact */
 function removeContact() {
-    console.log('Removing contact...');
+    
     // Add logic to handle removing a contact
+    let contactId = documentById('contactId').value;
+
+    console.log('Removing contact...');
+
+    if(!contactId) {
+        alert("Please provide a valid ContactID to remove.");
+        return;
+    }
+
 
     //Sending payload to PHP 
     let url = urlBase + "/DeleteContact." + extension;
@@ -187,6 +196,33 @@ function removeContact() {
 function editContact() {
     console.log('Editing contact...');
     // Add logic to handle editing a contact
+    
+
+    //Update all previous contact info:
+    //TODO: Determine if we want to implement unique ID for a contact,
+    //if so then we should update that also.
+    let contactId = document.getElementById('contactId').value; 
+    
+    let updatedName = document.getElementById('updatedName').value;
+    let updatedCompany = document.getElementById('updatedCompany').value;
+    let updatedEmail = document.getElementById('updatedEmail').value;
+    let updatedPhone = document.getElementById('updatedPhone').value;
+
+    console.log("Editing contact info");
+
+    //Make sure user input is valid:
+    if(!contactId || !updatedName || !updatedCompany || !updatedEmail || !updatedPhone){
+        alert('Please fill in all required fields.')
+    }
+
+    //Store all new data:
+    let userData = {
+        id: contactId,
+        name: updatedName,
+        company: updatedCompany,
+        email: updatedEmail,
+        phone: updatedPhone
+    };
 
 
     //Sending payload to PHP 
@@ -203,16 +239,17 @@ function editContact() {
             if (this.readyState == 4 && this.status == 200)
             {
                 console.log("Contact Edited");
+                loadContacts(); 
             }    
-        }
+        };
+        xhr.send(JSON.stringify(data)); 
     }
     catch(err)
     {
-        console.log(err.message);
+        console.log('Error trying to contact:', err.message);
     }
     
-    //will call load contact function at the end
-    loadContacts();
+    
 }
 
 /*Figure out logic behind redirecting user if they press return to login button */
