472c989b28135493a7d7beaba35d6065a628c2f0
Lucas-DeReamer <167575863+Lucas-DeReamer@users.noreply.github.com>
2024-09-11 23:44:27 -0400

    Update EditContact.php
    Now it has code :)

--- src/API/EditContact.php
+++ src/API/EditContact.php
@@ -1,6 +1,28 @@
 <?php
     $inData = getRequestInfo();
 
+    $cID = $inData["ID"];
+    $firstName = $inData["firstName"];
+    $lastName = $inData["lastName"];
+    $email = $inData["email"];
+    $phoneNumber = $inData["phoneNumber"];
+    $address = $inData["address"];
+
+    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "contact_manager");
+    if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+    else
+    {
+        $stmt = $conn->prepare("UPDATE Contacts SET firstName=? lastName=? email=? phoneNumber=? address=? WHERE ID=?");
+        $stmt->bind_param("sssssi", $firstName, $lastName, $email, $phoneNumber, $address, $cID);
+        $stmt->execute();
+	$stmt->close();
+	$conn->close();
+	returnWithError("Edit");
+    }
+
     
 
 
