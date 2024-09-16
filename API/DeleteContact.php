5ec05094262095537738696a1f59ceef82709819
calebmack <156357219+calebmack@users.noreply.github.com>
2024-09-16 14:30:28 -0400

    Update DeleteContact.php

--- API/DeleteContact.php
+++ API/DeleteContact.php
@@ -5,6 +5,8 @@
     $firstName = $inData["firstName"];
     $lastName = $inData["lastName"];
     $phoneNumber = $inData["phoneNumber"];
+    $email = $inData["email"];
+    $address = $inData["address"];
 
     $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "contact_manager");
     if( $conn->connect_error )
@@ -13,12 +15,12 @@
 	}
     else
     {
-        $stmt = $conn->prepare("DELETE FROM Contacts WHERE userID=? AND firstName=? AND lastName=? AND phoneNumber=?");
-        $stmt->bind_param("isss", $userID, $firstName, $lastName, $phoneNumber);
+        $stmt = $conn->prepare("DELETE FROM Contacts WHERE userID=? AND firstName=? AND lastName=? AND phoneNumber=? AND email=? AND address=?");
+        $stmt->bind_param("isssss", $userID, $firstName, $lastName, $phoneNumber, $email, $address);
         $stmt->execute();
 
         if ($stmt->affected_rows > 0) {
-            returnWithInfo( $firstName, $lastName, $phoneNumber );
+            returnWithInfo( $firstName, $lastName, $phoneNumber, $email, $address );
         }
         else {
             returnWithError("Contact not found");
@@ -47,9 +49,9 @@
 		sendResultInfoAsJson( $retValue );
 	}
 
-    function returnWithInfo( $firstName, $lastName, $phoneNumber)
+    function returnWithInfo( $firstName, $lastName, $phoneNumber, $email, $address)
 	{
-		$retValue = '{"phoneNumber":"' . $phoneNumber . '","firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+		$retValue = '{"phoneNumber":"' . $phoneNumber . '","firstName":"' . $firstName . '","lastName":"' . $lastName . '","email":"' . $email . '","address":"' . $address . '","error":""}';
 		sendResultInfoAsJson( $retValue );
 	}
 ?>
